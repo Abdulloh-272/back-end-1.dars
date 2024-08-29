106c7e04e9756d7702cc539a81483a1e351cdfff
Abdulloh-272 <167763951+Abdulloh-272@users.noreply.github.com>
2024-08-29 16:24:21 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+print('Fazliddinov Abdulloh')
+
+print('29-Sentabr 2008-yil') 
+
+print('Shayhontaxur Tumani Orifhon kochasi')
+
+print("Ozbekiston Respublikasi \"1-sentabr kuni\" mustaqillikka erishgan")
